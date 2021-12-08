3a6a56b638c091d42117f624cb7dabaff8471d48
赵志佳 <zhaozhijia@shiqiao.com>
2021-12-08 11:17:10 +0800

    chore: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  base:"/",
+  lang: 'zh-CN',
+  title: '来首有音乐治治我的腿！',
+  description: '这是·来首有音乐治治我的腿·站点',
+
+ themeConfig: {
+    navbar: [
+
+    ],
+    sidebar: [
+      // SidebarItem
+      {
+        text: '介绍',
+        link: '',
+      }
+    ],
+  },
+}
